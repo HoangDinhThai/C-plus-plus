e529a4f1a6b29c19d14639f74a67ce8c9318924e
HoangDinhThai <100067398+HoangDinhThai@users.noreply.github.com>
2022-04-02 23:05:31 +0700

    Add files via upload

---
+++ vector.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+void invec(vector<int> vec){
+    for(int i = 0; i < vec.size(); i++){
+        cout << vec.at(i) << endl;
+    }
+}
+
+int main(int argc, char *argv[]){
+    vector<int> vec;
+    // vec.resize(10, 0);
+    // vec[8] = 1;
+    // vec[2] = 9;
+    // vec.push_back(1);
+    // vec.push_back(2);
+    
+    for(int i = 0; i < ; i++)
+    invec(vec);
+    
+    return 0;
+}
